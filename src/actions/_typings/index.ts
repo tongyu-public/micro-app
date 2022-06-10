d16338c992b2a014d898414affdd14de75dd2a06
liqiujie <2274356148@qq.com>
2022-06-10 11:09:27 +0800

    feat: 删除actions

--- src/actions/_typings/index.ts
+++
@@ -1,6 +0,0 @@
-import type { _OSActionsCreateType } from './actions';
-import type { OSCustomFieldStaticPureTableFormFieldItemConfigsType } from '@ty-one-start/one-start';
-
-export type OSActionsCreateType =
-  _OSActionsCreateType<OSCustomFieldStaticPureTableFormFieldItemConfigsType>;
-export * from './actions';
